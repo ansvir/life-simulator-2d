cd2c7c062756f978ac03d24f4de0f15a44522e8a
tohant <svirepa.anton@gmail.com>
2023-02-26 21:21:22 +0100

    Finish timer

--- core/src/com/itique/ls2d/screen/MapScreen.java
+++ core/src/com/itique/ls2d/screen/MapScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
@@ -20,10 +21,7 @@ import com.badlogic.gdx.utils.IntSet;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.async.AsyncExecutor;
 import com.badlogic.gdx.utils.async.AsyncResult;
-import com.badlogic.gdx.utils.async.AsyncTask;
-import com.badlogic.gdx.utils.async.ThreadUtils;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.utils.viewport.*;
 import com.itique.ls2d.constant.world.DefaultCity;
 import com.itique.ls2d.model.Man;
 import com.itique.ls2d.model.timeline.TimeLineTask;
@@ -52,7 +50,8 @@ public class MapScreen implements Screen, InputProcessor {
     private static final int BORDER_DIAPASON = 5;
 
     private Game game;
-    private Stage stage;
+    private Stage mainStage;
+    private Stage controlStage;
     private Skin skin;
     private TextureAtlas atlas;
     private SpriteBatch batch;
@@ -72,32 +71,29 @@ public class MapScreen implements Screen, InputProcessor {
     private Map<Terrain, Pixmap> terrainPixmaps;
     private IntSet keys;
     private TimeLineTask timeline;
-    private AsyncExecutor asyncExecutor;
+    private final AsyncExecutor asyncExecutor;
     private AsyncResult<String> timeString;
+    private Music bgMusic;
 
     public MapScreen(Game game) {
         this.game = game;
-        stage = new Stage();
         atlas = new TextureAtlas(Gdx.files.internal("skins/plain-j/plain-james.atlas"));
         skin = new Skin(Gdx.files.internal("skins/plain-j/plain-james.json"), atlas);
         batch = new SpriteBatch();
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera.zoom = 0.8f;
         viewport = new ScreenViewport(camera);
-        stage = new Stage(viewport, batch);
-        personDao = new PersonFileDao();
-        worldDao = new WorldFileDao();
-        cityDao = new CityFileDao();
+        mainStage = new Stage(viewport, batch);
+        controlStage = new Stage();
         time = new Label("00:00", skin);
         controlTable = new Table();
-        controlTable.center();
-        controlTable.add(time).expand().top().right();
+        controlTable.setFillParent(true);
+        controlTable.add(time).pad(20).top().right();
         controlContainer = new Container<>(controlTable);
         controlContainer.setFillParent(true);
         controlContainer.center();
-        controlContainer.debugAll();
         keys = new IntSet();
         asyncExecutor = new AsyncExecutor(1);
+        bgMusic = Gdx.audio.newMusic(Gdx.files.internal("audio/map_bg.mp3"));
     }
 
     @Override
@@ -105,44 +101,31 @@ public class MapScreen implements Screen, InputProcessor {
         Gdx.input.setInputProcessor(this);
         timeline = new TimeLineTask(500L);
         timeString = asyncExecutor.submit(timeline);
-        hero = personDao.findHero().get();
-        world = worldDao.findAll().stream()
-                .filter(w -> w.getHumansIds().stream().anyMatch(h -> h.equals(hero.getId())))
-                .findFirst().get();
-        city = cityDao.findById(world.getCitiesIds().stream()
-                .filter(id -> id.equals(Gdx.app.getPreferences(GLOBAL_PREF).getString(HERO_START_CITY_ID_KEY)))
-                .findFirst().get()).get();
-        if (city.getDefaultId().equals(GREEN_ISLAND_CITY.name())) {
-            DefaultCity defaultCity = GREEN_ISLAND_CITY.getCity();
-            terrainPixmaps = defaultCity.getTerrainTexturesPaths()
-                    .entrySet().stream()
-                    .collect(Collectors.toMap(Map.Entry::getKey, v -> new Pixmap(Gdx.files.internal(v.getValue()))));
-            cityMap = new Pixmap(Gdx.files.internal(defaultCity.getMapPath()));
-            drawCityBorders();
-            cityMapTexture = new Texture(cityMap);
-            viewport.setWorldWidth(cityMap.getWidth());
-            viewport.setWorldHeight(cityMap.getHeight());
-        }
-        stage.addActor(controlContainer);
+        initializeData();
+        bgMusic.setLooping(true);
+        bgMusic.play();
+        controlStage.addActor(controlContainer);
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(Color.valueOf("178693"));
         Gdx.input.setInputProcessor(this);
+        processControlContainer();
         processTimLine();
         processKeyDown();
         mouseMoved(Gdx.input.getX(), Gdx.input.getY());
-        camera.update();
         batch.begin();
         batch.draw(cityMapTexture, 0, 0);
         batch.end();
-        stage.draw();
+        mainStage.draw();
+        controlStage.draw();
+        camera.update();
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height);
+        mainStage.getViewport().update(width, height);
     }
 
     @Override
@@ -162,7 +145,7 @@ public class MapScreen implements Screen, InputProcessor {
 
     @Override
     public void dispose() {
-        stage.dispose();
+        mainStage.dispose();
         skin.dispose();
         atlas.dispose();
         batch.dispose();
@@ -170,6 +153,8 @@ public class MapScreen implements Screen, InputProcessor {
         cityMap.dispose();
         cityMapTexture.dispose();
         timeline.forceFinish();
+        asyncExecutor.dispose();
+        bgMusic.dispose();
     }
 
     @Override
@@ -221,7 +206,6 @@ public class MapScreen implements Screen, InputProcessor {
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
-        System.out.println("screenX = " + screenX + ", screenY = " + screenY);
         processMapNavigation(screenX, screenY);
         return true;
     }
@@ -318,6 +302,33 @@ public class MapScreen implements Screen, InputProcessor {
         } else {
             timeline = new TimeLineTask(500L);
             timeString = asyncExecutor.submit(timeline);
+        }
+    }
+
+    private void processControlContainer() {
+        controlContainer.setPosition(viewport.getScreenX(),
+                viewport.getScreenY());
+    }
+
+    private void initializeData() {
+        personDao = new PersonFileDao();
+        worldDao = new WorldFileDao();
+        cityDao = new CityFileDao();
+        hero = personDao.findHero().get();
+        world = worldDao.findAll().stream()
+                .filter(w -> w.getHumansIds().stream().anyMatch(h -> h.equals(hero.getId())))
+                .findFirst().get();
+        city = cityDao.findById(world.getCitiesIds().stream()
+                .filter(id -> id.equals(Gdx.app.getPreferences(GLOBAL_PREF).getString(HERO_START_CITY_ID_KEY)))
+                .findFirst().get()).get();
+        if (city.getDefaultId().equals(GREEN_ISLAND_CITY.name())) {
+            DefaultCity defaultCity = GREEN_ISLAND_CITY.getCity();
+            terrainPixmaps = defaultCity.getTerrainTexturesPaths()
+                    .entrySet().stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, v -> new Pixmap(Gdx.files.internal(v.getValue()))));
+            cityMap = new Pixmap(Gdx.files.internal(defaultCity.getMapPath()));
+            drawCityBorders();
+            cityMapTexture = new Texture(cityMap);
         }
     }
 }
