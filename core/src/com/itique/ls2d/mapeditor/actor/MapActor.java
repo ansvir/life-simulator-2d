843029788656a2ff3b4c96e0ea64ad80950ea926
ansvir <svirepa.anton@gmail.com>
2023-04-09 22:54:06 +0200

    Update map and toolbar so map can move and toolbar responds to events, but keys are typed in inputs as well as map moves

--- core/src/com/itique/ls2d/mapeditor/actor/MapActor.java
+++ core/src/com/itique/ls2d/mapeditor/actor/MapActor.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Group;
@@ -17,8 +19,7 @@ import java.util.ArrayList;
 
 public class MapActor extends Group {
 
-    private float borderWidth = 5f;
-    private Color borderColor = Color.RED;
+    private static final int BORDER_DIAPASON = 5;
     private Texture backgroundTexture;
     private float width, height;
     private ArrayList<AbstractMapGroup> objects;
@@ -27,6 +28,7 @@ public class MapActor extends Group {
     private float panSpeed;
     private float zoom;
     private boolean moveLeft, moveRight, moveUp, moveDown;
+    private float currX, currY;
 
     public MapActor(Texture backgroundTexture, float width, float height, OrthographicCamera camera) {
         this.backgroundTexture = backgroundTexture;
@@ -39,35 +41,44 @@ public class MapActor extends Group {
         this.zoom = 1f;
         setSize(width, height);
         setOrigin(Align.center);
+        drawMapBorders();
         addListener(new InputListener() {
             @Override
             public boolean scrolled(InputEvent event, float x, float y, float amountX, float amountY) {
                 zoom += amountY * zoomSpeed;
                 zoom = MathUtils.clamp(zoom, 0.1f, 10f);
                 camera.zoom = zoom;
-                return true;
+                return false;
             }
 
             @Override
             public boolean keyDown(InputEvent event, int keycode) {
                 float delta = Gdx.graphics.getDeltaTime();
+                boolean isMoved = false;
                 if (keycode == Input.Keys.A) {
                     camera.translate(-panSpeed * delta * zoom, 0);
                     moveLeft = true;
+                    isMoved = true;
                 }
                 if (keycode == Input.Keys.D) {
                     camera.translate(panSpeed * delta * zoom, 0);
                     moveRight = true;
+                    isMoved = true;
                 }
                 if (keycode == Input.Keys.W) {
                     camera.translate(0, panSpeed * delta * zoom);
                     moveUp = true;
+                    isMoved = true;
                 }
                 if (keycode == Input.Keys.S) {
                     camera.translate(0, -panSpeed * delta * zoom);
                     moveDown = true;
+                    isMoved = true;
                 }
-                return true;
+                if (isMoved) {
+                    drawMapBorders();
+                }
+                return false;
             }
 
             @Override
@@ -86,25 +97,36 @@ public class MapActor extends Group {
                         moveDown = false;
                         break;
                 }
-                return true;
+                drawMapBorders();
+                return false;
             }
 
             @Override
             public boolean mouseMoved(InputEvent event, float x, float y) {
                 float margin = 50f;
+                float width = Gdx.graphics.getWidth();
+                float height = Gdx.graphics.getHeight();
+                boolean isMoved = false;
                 if (x < margin) {
                     camera.translate(-panSpeed * Gdx.graphics.getDeltaTime() * zoom, 0);
+                    isMoved = true;
                 }
                 if (x > width - margin) {
                     camera.translate(panSpeed * Gdx.graphics.getDeltaTime() * zoom, 0);
+                    isMoved = true;
                 }
-                if (x < margin) {
+                if (y < margin) {
                     camera.translate(0, panSpeed * Gdx.graphics.getDeltaTime() * zoom);
+                    isMoved = true;
                 }
-                if (x > height - margin) {
+                if (y > height - margin) {
                     camera.translate(0, -panSpeed * Gdx.graphics.getDeltaTime() * zoom);
+                    isMoved = true;
                 }
-                return true;
+                if (isMoved) {
+                    drawMapBorders();
+                }
+                return false;
             }
         });
     }
@@ -118,25 +140,40 @@ public class MapActor extends Group {
     public void draw(Batch batch, float parentAlpha) {
         batch.setColor(getColor().r, getColor().g, getColor().b, getColor().a * parentAlpha);
         batch.draw(backgroundTexture, getX(), getY(), getWidth(), getHeight());
+        Texture texture = new Texture("textures/book.png");
+        batch.draw(texture, getWidth() / 2, getHeight() / 2);
         super.draw(batch, parentAlpha);
         batch.end();
-
-        ShapeRenderer shapeRenderer = new ShapeRenderer();
-        shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
-        shapeRenderer.setTransformMatrix(batch.getTransformMatrix());
-        shapeRenderer.setColor(borderColor);
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.identity();
-        shapeRenderer.set(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.rect(getX() - borderWidth, getY() - borderWidth, getWidth() + borderWidth * 2, getHeight() + borderWidth * 2);
-        shapeRenderer.rectLine(getX(), getY(), getX(), getY() + getHeight(), borderWidth);
-        shapeRenderer.rectLine(getX() + getWidth(), getY(), getX() + getWidth(), getY() + getHeight(), borderWidth);
-        shapeRenderer.rectLine(getX(), getY(), getX() + getWidth(), getY(), borderWidth);
-        shapeRenderer.rectLine(getX(), getY() + getHeight(), getX() + getWidth(), getY() + getHeight(), borderWidth);
-        shapeRenderer.end();
-
         batch.begin();
+    }
+
+    public void drawMapBorders() {
+        if (!backgroundTexture.getTextureData().isPrepared()) {
+            backgroundTexture.getTextureData().prepare();
+        }
+        Pixmap pixmap = backgroundTexture.getTextureData().consumePixmap();
+        pixmap.setColor(Color.RED);
+        for (int y = 0; y <= backgroundTexture.getHeight(); y += BORDER_DIAPASON) {
+            int drawY = y;
+            pixmap.drawPixel(0, drawY++);
+            pixmap.drawPixel(0, drawY);
+        }
+        for (int y = 0; y <= backgroundTexture.getHeight(); y += BORDER_DIAPASON) {
+            int drawY = y;
+            pixmap.drawPixel(backgroundTexture.getWidth() - 1, drawY++);
+            pixmap.drawPixel(backgroundTexture.getWidth() - 1, drawY);
+        }
+        for (int x = 0; x <= backgroundTexture.getHeight(); x += BORDER_DIAPASON) {
+            int drawX = x;
+            pixmap.drawPixel(drawX++, 0);
+            pixmap.drawPixel(drawX, 0);
+        }
+        for (int x = 0; x <= backgroundTexture.getHeight(); x += BORDER_DIAPASON) {
+            int drawX = x;
+            pixmap.drawPixel(drawX++, backgroundTexture.getHeight() - 1);
+            pixmap.drawPixel(drawX, backgroundTexture.getHeight() - 1);
+        }
+        backgroundTexture = new Texture(pixmap);
     }
 
 }
--- core/src/com/itique/ls2d/mapeditor/screen/EditorScreen.java
+++ core/src/com/itique/ls2d/mapeditor/screen/EditorScreen.java
@@ -1,45 +1,31 @@
 package com.itique.ls2d.mapeditor.screen;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.itique.ls2d.constant.world.DefaultGreenCity;
-import com.itique.ls2d.custom.actor.CityMapActor;
-import com.itique.ls2d.custom.component.SplitComponent;
-import com.itique.ls2d.custom.component.style.MapStyle;
-import com.itique.ls2d.custom.component.style.ToolbarStyle;
-import com.itique.ls2d.custom.listener.ControlListener;
-import com.itique.ls2d.custom.listener.AbstractListener;
-import com.itique.ls2d.custom.listener.MapNavigationListener;
-import com.itique.ls2d.custom.listener.ToolbarListener;
 import com.itique.ls2d.mapeditor.actor.MapActor;
 import com.itique.ls2d.model.world.Terrain;
 
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.badlogic.gdx.Input.Keys.*;
-import static com.badlogic.gdx.Input.Keys.S;
 
-public class EditorScreen implements Screen {
+public class EditorScreen implements Screen, InputProcessor {
 
 //    private static final int BORDER_DIAPASON = 5;
 
@@ -55,10 +41,12 @@ public class EditorScreen implements Screen {
     private OrthographicCamera camera;
     private Viewport viewport;
     private Window toolBar;
-    private Stage stage;
-    private SplitComponent splitComponent;
+    private Stage mapStage;
+    private Stage uiStage;
     private MapActor mapActor;
     private Table toolbarTable;
+    private SpriteBatch batch;
+    private InputMultiplexer multiplexer;
 
     public EditorScreen(Game game) {
         this.game = game;
@@ -73,7 +61,9 @@ public class EditorScreen implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         viewport = new ScreenViewport(camera);
-        stage = new Stage();
+        batch = new SpriteBatch();
+        mapStage = new Stage(viewport, batch);
+        uiStage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
         mainContainer = new Table();
         mapPixmap = new Pixmap((int) mapWidth, (int) mapHeight, Pixmap.Format.RGBA8888);
         mapPixmap.setColor(Color.valueOf("178693"));
@@ -84,30 +74,51 @@ public class EditorScreen implements Screen {
                 .entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, v -> new Pixmap(Gdx.files.internal(v.getValue()))));
         createToolbar();
-        splitComponent = new SplitComponent(mapActor, toolBar);
-        mainContainer.add(splitComponent).expand().fill();
-        mainContainer.setFillParent(true);
-        mainContainer.debugAll();
-        stage.addActor(mainContainer);
-        stage.setViewport(viewport);
-        Gdx.input.setInputProcessor(stage);
+        mainContainer.add(toolBar).expand().fill().top().right();
+        mapStage.addActor(mapActor);
+        uiStage.addActor(mainContainer);
+        multiplexer = new InputMultiplexer(this, uiStage, mapStage);
+        Gdx.input.setInputProcessor(multiplexer);
     }
 
     @Override
     public void render(float delta) {
 //        mapNavigationListener.mouseMoved(Gdx.input.getX(), Gdx.input.getY());
 //        processKeyDown();
-        stage.act(delta);
-        camera.position.x = mainContainer.getX() + mainContainer.getWidth() / 2;
-        camera.position.y = mainContainer.getY() + mainContainer.getHeight() / 2;
+        ScreenUtils.clear(Color.valueOf("178693"));
+//        if (Gdx.input.getX() >= mainContainer.getX()
+//                && Gdx.input.getX() <= mainContainer.getX() + mainContainer.getWidth()
+//                && Gdx.input.getY() >= mainContainer.getY()
+//                && Gdx.input.getY() <= mainContainer.getY() + mainContainer.getHeight()) {
+//            Gdx.input.setInputProcessor(uiStage);
+//            System.out.println("toolbar");
+//        } else if (Gdx.input.getX() <= mainContainer.getX()
+//                && Gdx.input.getX() >= mainContainer.getX() + mainContainer.getWidth()
+//                && Gdx.input.getY() <= mainContainer.getY()
+//                && Gdx.input.getY() >= mainContainer.getY() + mainContainer.getHeight()) {
+//            Gdx.input.setInputProcessor(mapStage);
+//            System.out.println("MAP ACTOR");
+//        }
+        mouseMoved(Gdx.input.getX(), Gdx.input.getY());
+        int key = getKeyDown();
+        if (key != -1) {
+            keyDown(key);
+        }
+        mainContainer.setSize(toolBar.getWidth(), toolBar.getHeight());
+        batch.begin();
+        uiStage.act(delta);
+        mapStage.act(delta);
+//        camera.position.x = mainContainer.getX() + mainContainer.getWidth() / 2;
+//        camera.position.y = mainContainer.getY() + mainContainer.getHeight() / 2;
         camera.update();
-        ScreenUtils.clear(Color.WHITE);
-        stage.draw();
+        batch.end();
+        mapStage.draw();
+        uiStage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
+        mapStage.getViewport().update(width, height, true);
         camera.viewportWidth = width;
         camera.viewportHeight = height;
     }
@@ -134,8 +145,9 @@ public class EditorScreen implements Screen {
         terrainPixmaps.values().forEach(Pixmap::dispose);
         mapPixmap.dispose();
         mapTexture.dispose();
-        stage.dispose();
-        splitComponent.dispose();
+        mapStage.dispose();
+        uiStage.dispose();
+        batch.dispose();
     }
 
 //    private void drawCityBorders() {
@@ -162,26 +174,36 @@ public class EditorScreen implements Screen {
 //        }
 //    }
 
-//    private void processKeyDown() {
-//        int keyDown = Gdx.app.getInput().isKeyPressed(A) ? A
-//                : Gdx.app.getInput().isKeyPressed(W) ? W
-//                : Gdx.app.getInput().isKeyPressed(D) ? D
-//                : Gdx.app.getInput().isKeyPressed(S) ? S
-//                : -1;
-//        if (keyDown != -1) {
-//            mapNavigationListener.keyDown(keyDown);
-//        }
-//    }
-
     private void createToolbar() {
         Label mapNameLabel = new Label("NAME", skin);
         Label mapWidthLabel = new Label("WIDTH", skin);
         Label mapHeightLabel = new Label("HEIGHT", skin);
         TextField name = new TextField("some map 2", skin);
+        addTextChangeListener(name, true);
         TextField width = new TextField(String.valueOf(mapWidth), skin);
+        addTextChangeListener(width, false);
         TextField height = new TextField(String.valueOf(mapHeight), skin);
+        addTextChangeListener(height, false);
         TextButton exit = new TextButton("EXIT", skin);
+        exit.addListener(new ClickListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new MenuScreen(game));
+                return true;
+            }
+        });
         TextButton save = new TextButton("SAVE", skin);
+        save.addListener(new ClickListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                mapWidth = Integer.parseInt(width.getText());
+                mapHeight = Integer.parseInt(height.getText());
+                mapActor.setWidth(mapWidth);
+                mapActor.setHeight(mapHeight);
+                mapActor.drawMapBorders();
+                return true;
+            }
+        });
         TextButton load = new TextButton("LOAD", skin);
         float pad = 10;
         toolbarTable = new Table();
@@ -204,10 +226,134 @@ public class EditorScreen implements Screen {
         toolbarTable.add(exit).padLeft(pad)
                 .padRight(pad).expandX().fill().right();
         toolBar = new Window("Toolbar", skin);
-        toolBar.setResizable(false);
+        toolBar.setResizable(true);
         toolBar.setModal(false);
-        toolBar.setMovable(false);
-        toolBar.add(toolbarTable).expand().fill().top();
+        toolBar.setMovable(true);
+        toolBar.add(toolbarTable).expand().fill();
+        toolBar.setSize(toolBar.getPrefWidth(), toolBar.getPrefHeight());
     }
-    
+
+    private int getKeyDown() {
+        return Gdx.app.getInput().isKeyPressed(A) ? A
+                : Gdx.app.getInput().isKeyPressed(W) ? W
+                : Gdx.app.getInput().isKeyPressed(D) ? D
+                : Gdx.app.getInput().isKeyPressed(S) ? S
+                : -1;
+    }
+
+    private boolean isHoverToolbar() {
+        System.out.println(Gdx.input.getX() >= mainContainer.getX()
+                && Gdx.input.getX() <= mainContainer.getX() + mainContainer.getWidth()
+                && Gdx.input.getY() >= mainContainer.getY()
+                && Gdx.input.getY() <= mainContainer.getY() - mainContainer.getHeight());
+        return Gdx.input.getX() >= mainContainer.getX()
+                && Gdx.input.getX() <= mainContainer.getX() + mainContainer.getWidth()
+                && Gdx.input.getY() >= mainContainer.getY()
+                && Gdx.input.getY() <= mainContainer.getY() - mainContainer.getHeight();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        int key = getKeyDown();
+        if (key != -1 && !isHoverToolbar()) {
+            InputEvent keyTyped = new InputEvent();
+            keyTyped.setType(InputEvent.Type.keyDown);
+            keyTyped.setStage(mapStage);
+            keyTyped.setKeyCode(key);
+            mapActor.fire(keyTyped);
+        }
+        if (isHoverToolbar()) {
+            InputEvent keyTyped = new InputEvent();
+            keyTyped.setType(InputEvent.Type.keyDown);
+            keyTyped.setStage(uiStage);
+            keyTyped.setKeyCode(key);
+            mainContainer.fire(keyTyped);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        int key = getKeyDown();
+        if (key != -1 && !isHoverToolbar()) {
+            InputEvent keyUp = new InputEvent();
+            keyUp.setType(InputEvent.Type.keyUp);
+            keyUp.setStage(mapStage);
+            keyUp.setKeyCode(key);
+            mapActor.fire(keyUp);
+        }
+        if (isHoverToolbar()) {
+            InputEvent keyUp = new InputEvent();
+            keyUp.setType(InputEvent.Type.keyUp);
+            keyUp.setStage(uiStage);
+            keyUp.setKeyCode(keycode);
+            mainContainer.fire(keyUp);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        if (!isHoverToolbar()) {
+            InputEvent mouseMoved = new InputEvent();
+            mouseMoved.setType(InputEvent.Type.mouseMoved);
+            mouseMoved.setStage(mapStage);
+            mouseMoved.setStageX(screenX);
+            mouseMoved.setStageY(screenY);
+            mapActor.fire(mouseMoved);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        if (!isHoverToolbar()) {
+            InputEvent scrolled = new InputEvent();
+            scrolled.setType(InputEvent.Type.scrolled);
+            scrolled.setStage(mapStage);
+            scrolled.setStageX(Gdx.input.getX());
+            scrolled.setStageY(Gdx.input.getY());
+            scrolled.setScrollAmountY(amountY);
+            mapActor.fire(scrolled);
+        }
+        return true;
+    }
+
+    private void addTextChangeListener(TextField field, boolean text) {
+        field.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                if (!isHoverToolbar()) {
+                    event.cancel();
+                }
+                if (!text) {
+                    try {
+                        Long.parseLong(field.getText());
+                    } catch (NumberFormatException e) {
+                        event.cancel();
+                    }
+                }
+            }
+        });
+    }
 }
